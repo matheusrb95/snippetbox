453b1b0d1695bbab092974646d6307d345ab9c75
matheusrb95 <matheusrenan.95@gmail.com>
2026-06-12 06:03:52 -0300

    chore: add base html template

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -9,14 +9,21 @@ import (
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
-	ts, err := template.ParseFiles("./ui/html/pages/home.tmpl.html")
+	w.Header().Add("Server", "Go")
+
+	files := []string{
+		"./ui/html/base.tmpl.html",
+		"./ui/html/pages/home.tmpl.html",
+	}
+
+	ts, err := template.ParseFiles(files...)
 	if err != nil {
 		log.Print(err.Error())
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 		return
 	}
 
-	if err := ts.Execute(w, nil); err != nil {
+	if err := ts.ExecuteTemplate(w, "base", nil); err != nil {
 		log.Print(err.Error())
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 	}
