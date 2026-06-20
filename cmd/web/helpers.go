0c5b6356420cfe69b1ba4c2de37e7e88b73cbaf7
matheusrb95 <matheusrenan.95@gmail.com>
2026-06-20 06:53:40 -0300

    chore: render template if error send internal server error

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"net/http"
 	"runtime/debug"
@@ -29,10 +30,14 @@ func (app *application) render(w http.ResponseWriter, r *http.Request, status in
 		return
 	}
 
+	buf := new(bytes.Buffer)
+
+	if err := ts.ExecuteTemplate(buf, "base", data); err != nil {
+		app.serverError(w, r, err)
+		return
+	}
+
 	w.WriteHeader(status)
 
-	err := ts.ExecuteTemplate(w, "base", data)
-	if err != nil {
-		app.serverError(w, r, err)
-	}
+	buf.WriteTo(w)
 }
