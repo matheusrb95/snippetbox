63cbd437fa27ad964eee3bed2adb1b43d869e254
matheusrb95 <matheusrenan.95@gmail.com>
2026-06-21 06:12:31 -0300

    feat: accept input by user in front to create new snippet

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -52,9 +52,20 @@ func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request) {
-	title := "O snail"
-	content := "O snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n– Kobayashi Issa"
-	expires := 7
+	r.Body = http.MaxBytesReader(w, r.Body, 4096)
+
+	if err := r.ParseForm(); err != nil {
+		app.clientError(w, http.StatusBadRequest)
+		return
+	}
+
+	title := r.PostForm.Get("title")
+	content := r.PostForm.Get("content")
+	expires, err := strconv.Atoi(r.PostForm.Get("expires"))
+	if err != nil {
+		app.clientError(w, http.StatusBadRequest)
+		return
+	}
 
 	id, err := app.snippets.Insert(title, content, expires)
 	if err != nil {
