acf3741386186c85026db2fcd06cc714a50645c9
matheusrb95 <matheusrenan.95@gmail.com>
2026-06-22 05:19:33 -0300

    refactor: move session into new template instead of handler

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -20,6 +20,7 @@ type templateData struct {
 func (app *application) newTemplateData(r *http.Request) templateData {
 	return templateData{
 		CurrentYear: time.Now().Year(),
+		Flash:       app.sessionManager.PopString(r.Context(), "flash"),
 	}
 }
 
