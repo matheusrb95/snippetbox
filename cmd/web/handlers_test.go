868c9a79fdcf2c0362e660be6ac6886b67534295
matheusrb95 <matheusrenan.95@gmail.com>
2026-07-02 06:01:26 -0300

    chore: improve ping handler test using a test server

--- cmd/web/handlers_test.go
+++ cmd/web/handlers_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"io"
+	"log/slog"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -11,16 +12,18 @@ import (
 )
 
 func TestPing(t *testing.T) {
-	rr := httptest.NewRecorder()
+	app := &application{
+		logger: slog.New(slog.DiscardHandler),
+	}
 
-	r, err := http.NewRequest(http.MethodGet, "/", nil)
+	ts := httptest.NewTLSServer(app.routes())
+	defer ts.Close()
+
+	rs, err := ts.Client().Get(ts.URL + "/ping")
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	ping(rr, r)
-
-	rs := rr.Result()
 	assert.Equal(t, rs.StatusCode, http.StatusOK)
 
 	body, err := io.ReadAll(rs.Body)
